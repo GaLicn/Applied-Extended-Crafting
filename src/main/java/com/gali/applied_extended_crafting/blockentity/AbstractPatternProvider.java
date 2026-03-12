c49b65d413d1f740a957cae9989d364bea102a27
GaLi <3096147684@qq.com>
2026-03-12 11:19:35 +0800

    增加pendingOutputs，用于pushPatetern后的cpu等待回调

--- src/main/java/com/gali/applied_extended_crafting/blockentity/AbstractPatternProvider.java
+++ src/main/java/com/gali/applied_extended_crafting/blockentity/AbstractPatternProvider.java
@@ -2,23 +2,36 @@ package com.gali.applied_extended_crafting.blockentity;
 
 import appeng.api.config.Actionable;
 import appeng.api.crafting.IPatternDetails;
+import appeng.api.networking.IGridNode;
 import appeng.api.networking.security.IActionHost;
 import appeng.api.networking.security.IActionSource;
 import appeng.api.stacks.AEKey;
 import appeng.api.stacks.GenericStack;
+import appeng.blockentity.ServerTickingBlockEntity;
 import appeng.blockentity.crafting.PatternProviderBlockEntity;
 import appeng.helpers.patternprovider.PatternProviderLogic;
 import com.gali.applied_extended_crafting.recipe.IRecipeMatcher;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Optional;
 
-public abstract class AbstractPatternProvider extends PatternProviderBlockEntity implements IActionHost {
+public abstract class AbstractPatternProvider extends PatternProviderBlockEntity
+        implements IActionHost, ServerTickingBlockEntity {
+    private static final String NBT_PENDING_OUTPUTS = "pendingOutputs";
+
+    private final List<GenericStack> pendingOutputs = new ArrayList<>();
+
     protected AbstractPatternProvider(BlockEntityType<?> blockEntityType, BlockPos pos, BlockState state) {
         super(blockEntityType, pos, state);
     }
@@ -35,7 +48,7 @@ public abstract class AbstractPatternProvider extends PatternProviderBlockEntity
     }
 
     @Override
-    public appeng.api.networking.IGridNode getActionableNode() {
+    public IGridNode getActionableNode() {
         return this.getMainNode().getNode();
     }
 
@@ -63,22 +76,75 @@ public abstract class AbstractPatternProvider extends PatternProviderBlockEntity
             return false;
         }
 
+        this.pendingOutputs.addAll(patternDetails.getOutputs());
+        this.saveChanges();
+
+        return true;
+    }
+
+    @Override
+    public void serverTick() {
+        if (this.pendingOutputs.isEmpty()) {
+            return;
+        }
+
+        var level = this.getLevel();
+        if (level == null || level.isClientSide()) {
+            return;
+        }
+
+        var grid = this.getMainNode().getGrid();
+        if (grid == null) {
+            return;
+        }
+
         var storage = grid.getStorageService().getInventory();
         var actionSource = IActionSource.ofMachine(this);
-        List<GenericStack> outputs = patternDetails.getOutputs();
+        boolean changed = false;
 
-        for (var output : outputs) {
-            long inserted = storage.insert(output.what(), output.amount(), Actionable.SIMULATE, actionSource);
-            if (inserted != output.amount()) {
-                return false;
+        for (ListIterator<GenericStack> iterator = this.pendingOutputs.listIterator(); iterator.hasNext();) {
+            var pendingOutput = iterator.next();
+            long inserted = storage.insert(pendingOutput.what(), pendingOutput.amount(), Actionable.MODULATE,
+                    actionSource);
+            if (inserted <= 0) {
+                continue;
+            }
+
+            changed = true;
+            if (inserted >= pendingOutput.amount()) {
+                iterator.remove();
+            } else {
+                iterator.set(new GenericStack(pendingOutput.what(), pendingOutput.amount() - inserted));
             }
         }
 
-        for (var output : outputs) {
-            storage.insert(output.what(), output.amount(), Actionable.MODULATE, actionSource);
+        if (changed) {
+            this.saveChanges();
         }
+    }
 
-        return true;
+    @Override
+    public void saveAdditional(CompoundTag data, HolderLookup.Provider registries) {
+        super.saveAdditional(data, registries);
+
+        ListTag pendingOutputsTag = new ListTag();
+        for (var pendingOutput : this.pendingOutputs) {
+            pendingOutputsTag.add(GenericStack.writeTag(registries, pendingOutput));
+        }
+        data.put(NBT_PENDING_OUTPUTS, pendingOutputsTag);
+    }
+
+    @Override
+    public void loadTag(CompoundTag data, HolderLookup.Provider registries) {
+        super.loadTag(data, registries);
+
+        this.pendingOutputs.clear();
+        for (Tag pendingOutputTag : data.getList(NBT_PENDING_OUTPUTS, Tag.TAG_COMPOUND)) {
+            var pendingOutput = GenericStack.readTag(registries, (CompoundTag) pendingOutputTag);
+            if (pendingOutput != null) {
+                this.pendingOutputs.add(pendingOutput);
+            }
+        }
     }
 
     protected Optional<Map<AEKey, Long>> extractInputs(IPatternDetails patternDetails) {
