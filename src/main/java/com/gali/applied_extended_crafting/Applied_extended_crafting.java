2b3e5b88c881ff2a5dd77670925064b47ebd75d8
GaLi <3096147684@qq.com>
2026-03-13 11:45:18 +0800

    init commit

---
+++ src/main/java/com/gali/applied_extended_crafting/Applied_extended_crafting.java
@@ -0,0 +1,9 @@
+package com.gali.applied_extended_crafting;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Applied_extended_crafting.MODID)
+public class Applied_extended_crafting {
+
+    public static final String MODID = "applied_extended_crafting";
+}
