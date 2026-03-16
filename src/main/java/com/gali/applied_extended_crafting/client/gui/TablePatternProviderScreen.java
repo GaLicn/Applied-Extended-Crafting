533eac74ef934fcd9abe8d3af5807d1efe4f0a85
GaLi <3096147684@qq.com>
2026-03-16 16:59:25 +0800

    1.1.1-优化工作台合成站的ui

--- src/main/java/com/gali/applied_extended_crafting/client/gui/TablePatternProviderScreen.java
+++ src/main/java/com/gali/applied_extended_crafting/client/gui/TablePatternProviderScreen.java
@@ -1,12 +1,12 @@
 package com.gali.applied_extended_crafting.client.gui;
 
-import appeng.client.gui.implementations.PatternProviderScreen;
+import appeng.client.gui.AEBaseScreen;
 import appeng.client.gui.style.ScreenStyle;
 import com.gali.applied_extended_crafting.menu.TablePatternProviderMenu;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 
-public class TablePatternProviderScreen extends PatternProviderScreen<TablePatternProviderMenu> {
+public class TablePatternProviderScreen extends AEBaseScreen<TablePatternProviderMenu> {
     public TablePatternProviderScreen(TablePatternProviderMenu menu, Inventory playerInventory, Component title,
                                       ScreenStyle style) {
         super(menu, playerInventory, title, style);
